43509aa2a1547198914a40691dfbc147ffd972c3
Anu Gill <gillanu2003@gmail.com>
2025-11-11 18:51:23 +0530

    linearSearch method

--- Linearsearch.java
+++ Linearsearch.java
@@ -2,12 +2,11 @@ public class LinearSearchExample {
     public static int linearSearch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
-                return i; // return index if found
+                return i; 
             }
         }
-        return -1; // return -1 if not found
+        return -1; 
     }
-
     public static void main(String[] args) {
         int[] arr = {10, 23, 45, 12, 7};
         int target = 12;
