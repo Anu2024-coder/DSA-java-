90d0099c69ecc7f9ba817945912b5ad6de3daee2
Anu Gill <gillanu2003@gmail.com>
2025-11-13 19:36:38 +0530

    Add string reversal example using StringBuilder

---
+++ reverseUsingStringBuilder.java
@@ -0,0 +1,9 @@
+public class ReverseStringExample2 {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        StringBuilder sb = new StringBuilder(str);
+        
+        sb.reverse();
+        System.out.println("Reversed String: " + sb);
+    }
+}
