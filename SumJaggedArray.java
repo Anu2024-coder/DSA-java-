c7a51e095b918fd7c0fea24538ab1c575fbe148d
Anu Gill <gillanu2003@gmail.com>
2025-11-13 19:25:18 +0530

    sum of elements in jagged array

---
+++ SumJaggedArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class JaggedArraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] jagged = new int[3][];
+        jagged[0] = new int[2];  
+        jagged[1] = new int[3]; 
+        jagged[2] = new int[1];  
+        System.out.println("Enter elements for the jagged array:");
+        for (int i = 0; i < jagged.length; i++) {
+            for (int j = 0; j < jagged[i].length; j++) {
+                System.out.print("Enter element at [" + i + "][" + j + "]: ");
+                jagged[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("\nJagged Array:");
+        for (int i = 0; i < jagged.length; i++) {
+            for (int j = 0; j < jagged[i].length; j++) {
+                System.out.print(jagged[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int sum = 0;
+        for (int i = 0; i < jagged.length; i++) {
+            for (int j = 0; j < jagged[i].length; j++) {
+                sum += jagged[i][j];
+            }
+        }
+        System.out.println("\nSum of all elements = " + sum);
+
+        sc.close();
+    }
+}
